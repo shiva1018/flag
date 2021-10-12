ee85aff03c668c07b5b5820fced17b431ae063c6
shiva1018 <91366925+shiva1018@users.noreply.github.com>
2021-10-12 14:50:53 +0530

    flag

---
+++ flag.js
@@ -0,0 +1,40 @@
+function flag() {
+    var x=document.createElement("img");
+    x.setAttribute("src","https://flagpedia.net/data/flags/w580/us.png")
+    x.setAttribute("width","256")
+     x.setAttribute("height","399")
+    x.setAttribute("alt","india")
+    document.body.appendChild(x);
+     var x1=document.createElement("img"); x1.setAttribute("src","https://flagpedia.net/data/flags/w580/kr.png")
+    x1.setAttribute("width","256")
+     x1.setAttribute("height","399")
+    x1.setAttribute("alt","india")
+    document.body.appendChild(x1);
+      var x2=document.createElement("img"); x2.setAttribute("src","https://flagpedia.net/data/flags/w580/in.png")
+    x2.setAttribute("width","256")
+     x2.setAttribute("height","399")
+    x2.setAttribute("alt","india")
+    document.body.appendChild(x2);
+    x3=document.createElement("img"); x3.setAttribute("src","https://flagpedia.net/data/flags/w580/au.png")
+    x3.setAttribute("width","256")
+     x3.setAttribute("height","399")
+    x3.setAttribute("alt","india")
+    document.body.appendChild(x3);
+     x4=document.createElement("img"); x4.setAttribute("src","https://flagpedia.net/data/flags/w580/de.png")
+    x4.setAttribute("width","256")
+     x4.setAttribute("height","399")
+    x4.setAttribute("alt","india")
+    document.body.appendChild(x4);
+    
+  }
+   flag();
+  // const para2=document.createElement("div")
+  // document.body.appendChild(para2)
+  const para=document.createElement("p");
+  var y=document.createTextNode("Country: United States of America")
+  para.appendChild(y);
+   document.body.appendChild(para)
+  const para1=document.createElement("p");
+  var y1=document.createTextNode("population: 323,947,000")
+  para.appendChild(y1);
+  document.body.appendChild(para1)
